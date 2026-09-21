0a90449686239a4ae5feab1ffc804b9df18909ca
ruthistanbull-netizen <ruthistanbull@gmail.com>
2026-09-21 16:59:35 +0300

    feat: add Rosta default hero asset

---
+++ apps/storefront/src/lib/rostaHeroAsset.ts
@@ -0,0 +1,7 @@
+import chunk0 from "@/lib/rostaHeroCurrentChunks/000";
+import chunk1 from "@/lib/rostaHeroCurrentChunks/001";
+import chunk2 from "@/lib/rostaHeroCurrentChunks/002";
+import chunk3 from "@/lib/rostaHeroCurrentChunks/003";
+
+export const ROSTA_DEFAULT_HERO_IMAGE =
+  `data:image/webp;base64,${chunk0}${chunk1}${chunk2}${chunk3}`;
