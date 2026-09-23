d5eb8fa5eb0f1d6575b9fb7ef85b30ab5ae9f30e
ruthistanbull-netizen <ruthistanbull@gmail.com>
2026-09-23 20:01:45 +0300

    chore(db): remove superseded migration timestamp 20260922215500_rosta_push_fk_indexes.sql

--- supabase/migrations/20260922215500_rosta_push_fk_indexes.sql
+++
@@ -1,5 +0,0 @@
-create index if not exists admin_push_jobs_order_id_idx
-  on public.admin_push_jobs(order_id);
-
-create index if not exists admin_push_subscriptions_admin_profile_id_idx
-  on public.admin_push_subscriptions(admin_profile_id);
